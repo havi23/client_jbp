bd9feb251068f9dc9b7e9d9a3844430475a12c35
havi23 <39590414+havi23@users.noreply.github.com>
2019-07-31 00:18:25 +0300

    v0.28

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 def get_content(path, character_name, server):
-    with open(f'{path}\\TellMeWhen.lua', 'r', encoding='utf-8') as tmw_config:
+    with open(f'{path}\\TellMeWhen1.lua', 'r', encoding='utf-8') as tmw_config:
         tmw_config = tmw_config.read()
         if 'TellMeWhenDB' in tmw_config:
             # Эта строка в файле встречается дважды. ИМЯПЕРСОНАЖА - СЕРВЕР.
             character_string = f'{character_name} - {server}'
             # Если есть конфиг на персонажа
             if character_string in tmw_config:  # Old config was found
-                content = tmw_config.split(f'["{character_name} - {server}"] = {{')[1]
-                content = content.split('\n\t\t},')[0]
-                return content
+                profile_name = tmw_config.split(f'["{character_name} - {server}"] = "')[1].split(',')[0][:-1]
+                print(profile_name)
+                profile = tmw_config.split(f'["{profile_name}"] = {{')[1]
+                profile = profile.split('\n\t\t},')[0]
+                return profile
             else:  # This is first run
                 pass
 
@@ -23,15 +25,15 @@ def generate_config():
 def replace(character_name, server, path_from, path_to):
     with open(f'{path_from}', 'r', encoding='utf-8') as new_profile:
         new_profile = new_profile.read()
-    with open(f'{path_to}\\TellMeWhen.lua', 'r', encoding='utf-8') as content:
+    with open(f'{path_to}\\TellMeWhen1.lua', 'r', encoding='utf-8') as content:
         content = content.read()
     old_profile = get_content(path_to, character_name, server)
-    with open(f'{path_to}\\TellMeWhen.lua', 'w', encoding='utf-8') as new_file:
+    with open(f'{path_to}\\TellMeWhen1.lua', 'w', encoding='utf-8') as new_file:
         print(old_profile)
         new_file.write(content.replace(old_profile, new_profile))
 
 character_name = 'Тестовыйф'
 server = 'Страж Смерти'
-path_from = 'C:\\Users\\Max\\Documents\\0 Дела\\jbp\\Генератор TMW\\tmw_file.lua'
+path_from = 'C:\\Users\\Max\\Documents\\0 Дела\\jbp\\Генератор TMW\\profile.lua'
 path_to = 'C:\\Users\\Max\\Documents\\0 Дела\\jbp\\Генератор TMW'
 replace(character_name, server, path_from, path_to)
