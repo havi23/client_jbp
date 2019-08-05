46a4128030907cc06300580a4ab34de362f021ee
havi23 <39590414+havi23@users.noreply.github.com>
2019-08-05 17:28:48 +0300

    Add files via upload

--- bin/Qt/license_key_Qt.py
+++ bin/Qt/license_key_Qt.py
@@ -7,7 +7,6 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from bin.resource_to_exe import resource_path
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
@@ -16,41 +15,39 @@ class Ui_Dialog(object):
         self.bg = QtWidgets.QLabel(Dialog)
         self.bg.setGeometry(QtCore.QRect(10, 10, 291, 141))
         self.bg.setText("")
-        self.bg.setPixmap(QtGui.QPixmap(resource_path("bin\\img\\license\\license_key.png")))
+        self.bg.setPixmap(QtGui.QPixmap("bin/img/license/bg.png"))
         self.bg.setObjectName("bg")
         self.key_edit = QtWidgets.QLineEdit(Dialog)
-        self.key_edit.setGeometry(QtCore.QRect(24, 86, 188, 20))
+        self.key_edit.setGeometry(QtCore.QRect(25, 63, 188, 20))
         self.key_edit.setObjectName("key_edit")
-        self.description = QtWidgets.QLabel(Dialog)
-        self.description.setGeometry(QtCore.QRect(80, 65, 146, 16))
-        font = QtGui.QFont()
-        font.setFamily("Futura-Normal")
-        font.setPointSize(12)
-        self.description.setFont(font)
-        self.description.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.description.setStyleSheet("color: silver")
-        self.description.setAlignment(QtCore.Qt.AlignCenter)
-        self.description.setObjectName("description")
         self.submit = QtWidgets.QLabel(Dialog)
-        self.submit.setGeometry(QtCore.QRect(222, 88, 61, 16))
+        self.submit.setGeometry(QtCore.QRect(218, 61, 62, 25))
+        self.submit.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
         self.submit.setText("")
+        self.submit.setPixmap(QtGui.QPixmap("bin/img/license/submit.png"))
         self.submit.setObjectName("submit")
         self.website = QtWidgets.QLabel(Dialog)
-        self.website.setGeometry(QtCore.QRect(76, 118, 149, 18))
+        self.website.setGeometry(QtCore.QRect(229, 35, 63, 21))
+        self.website.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
         self.website.setText("")
+        self.website.setPixmap(QtGui.QPixmap("bin/img/license/website.png"))
         self.website.setObjectName("website")
         self.error = QtWidgets.QLabel(Dialog)
-        self.error.setGeometry(QtCore.QRect(177, 118, 101, 16))
+        self.error.setGeometry(QtCore.QRect(23, 91, 257, 46))
         font = QtGui.QFont()
         font.setFamily("Futura-Normal")
         font.setPointSize(12)
         self.error.setFont(font)
+        self.error.setContextMenuPolicy(QtCore.Qt.DefaultContextMenu)
         self.error.setLayoutDirection(QtCore.Qt.LeftToRight)
         self.error.setStyleSheet("color: red")
+        self.error.setScaledContents(False)
         self.error.setAlignment(QtCore.Qt.AlignCenter)
+        self.error.setWordWrap(True)
         self.error.setObjectName("error")
         self.exit_ = QtWidgets.QLabel(Dialog)
         self.exit_.setGeometry(QtCore.QRect(250, 17, 38, 17))
+        self.exit_.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
         self.exit_.setText("")
         self.exit_.setObjectName("exit_")
 
@@ -60,6 +57,5 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.description.setText(_translate("Dialog", "Enter a license key"))
         self.error.setText(_translate("Dialog", "wrong key"))
 
